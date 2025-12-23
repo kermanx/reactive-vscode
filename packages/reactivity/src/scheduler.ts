43983a9847c131ca93a19010044d61523d5a9ed5
_Kerman <kermanx@qq.com>
2025-12-23 19:10:58 +0800

    chore: update forked vue api

--- packages/reactivity/src/scheduler.ts
+++ packages/reactivity/src/scheduler.ts
@@ -47,12 +47,16 @@ let currentFlushPromise: Promise<void> | null = null
 const RECURSION_LIMIT = 100
 type CountMap = Map<SchedulerJob, number>
 
-export function nextTick<T = void, R = void>(
+export function nextTick(): Promise<void>
+export function nextTick<T, R>(
   this: T,
-  fn?: (this: T) => R,
-): Promise<Awaited<R>> {
+  fn: (this: T) => R | Promise<R>,
+): Promise<R>
+export function nextTick<T, R>(
+  this: T,
+  fn?: (this: T) => R | Promise<R>,
+): Promise<void | R> {
   const p = currentFlushPromise || resolvedPromise
-  // @ts-expect-error ???
   return fn ? p.then(this ? fn.bind(this) : fn) : p
 }
 
